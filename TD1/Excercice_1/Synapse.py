18fe28c7cfd0a93deb033c49b3c9ed5108dc915f
alexiscampan <alexis.campan@ut-capitole.fr>
2022-09-06 11:56:40 +0200

    synapses added

--- TD1/Excercice_1/Synapse.py
+++ TD1/Excercice_1/Synapse.py
@@ -1,2 +1,14 @@
-class Synapse :
-    pass 
+"""Synapse
+    Synapse contenant le poids synaptique et le neurone entrant et le sortant
+"""
+class Synapse:
+    def __init__(self, weight, n_in, n_out):
+        self.weight = weight
+        self.n_in = n_in
+        self.n_out = n_out
+    
+    def get_weight(self):
+        return self.weight
+    
+    def set_weight(self, val):
+        self.weight = val
